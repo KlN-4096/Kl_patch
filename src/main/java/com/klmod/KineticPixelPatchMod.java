019945f747534e55170c67575c24e1215122462e
KlNon <y569034941@gmail.com>
2024-10-04 22:09:08 +0800

    init

---
+++ src/main/java/com/klmod/KineticPixelPatchMod.java
@@ -0,0 +1,25 @@
+package com.klmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("kinetic_pixel_patch")
+public class KineticPixelPatchMod {
+
+    public static final String MODID = "kinetic_pixel_patch";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public KineticPixelPatchMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
+}
